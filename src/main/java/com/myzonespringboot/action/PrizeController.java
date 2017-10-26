ffebd9478197941834ff154a190862679919cd58
chengq <2881815605@qq.com>
2017-10-26 16:59:25 +0800

    数据访问层基类

--- src/main/java/com/myzonespringboot/action/PrizeController.java
+++ src/main/java/com/myzonespringboot/action/PrizeController.java
@@ -1,5 +1,6 @@
 package com.myzonespringboot.action;
 
+import com.myzonespringboot.model.Prize;
 import com.myzonespringboot.model.User;
 import com.myzonespringboot.util.Message;
 import org.springframework.stereotype.Controller;
@@ -12,6 +13,7 @@ import javax.servlet.http.HttpSession;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Controller("prizeManageController")
@@ -21,55 +23,66 @@ public class PrizeController {
     private HttpSession session;
 
     @RequestMapping(method = RequestMethod.GET)
-    public @ResponseBody Map<String, Object> prize() {
+    public @ResponseBody
+    Map<String, Object> prize() {
         User user = (User) session.getAttribute("user");
-        if (user == null) {
-            return Message.failure("请先登录!");
-        }
-        return Message.success(startPrize());
+        return user == null ? Message.failure("请先登录!") : Message.success(startPrize());
     }
 
     /**
      * 开始抽奖
+     *
      * @return
      */
-    private Map<String,Object> startPrize(){
-        Map<String,Object> prizeMsg=new HashMap<String, Object>();
+    private Map<String, Object> startPrize() {
+        Map<String, Object> prizeMsg = new HashMap<String, Object>();
         int num = (int) (Math.random() * 10000 + 1);//产生1~10001随机数 [1,10001)
-        String date=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
+        String date = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
         if (num == 1) {
-            prizeMsg.put("msg","恭喜获得一等奖!");
-            prizeMsg.put("choice",1);
-            prizeMsg.put("content","恭喜获得一等奖! " + date);
+            prizeMsg.put("msg", "恭喜获得一等奖!");
+            prizeMsg.put("choice", 1);
+            prizeMsg.put("content", "恭喜获得一等奖! " + date);
         } else if (num > 1 && num <= 10) {
-            prizeMsg.put("msg","恭喜获得二等奖!");
-            prizeMsg.put("choice",2);
-            prizeMsg.put("content","恭喜获得二等奖! " + date);
+            prizeMsg.put("msg", "恭喜获得二等奖!");
+            prizeMsg.put("choice", 2);
+            prizeMsg.put("content", "恭喜获得二等奖! " + date);
         } else if (num > 10 && num <= 110) {
-            prizeMsg.put("msg","恭喜获得三等奖!");
-            prizeMsg.put("choice",3);
-            prizeMsg.put("content","恭喜获得三等奖! " + date);
+            prizeMsg.put("msg", "恭喜获得三等奖!");
+            prizeMsg.put("choice", 3);
+            prizeMsg.put("content", "恭喜获得三等奖! " + date);
         } else if (num > 110 && num <= 310) {
-            prizeMsg.put("msg","恭喜获得四等奖!");
-            prizeMsg.put("choice",4);
-            prizeMsg.put("content","恭喜获得四等奖! " + date);
+            prizeMsg.put("msg", "恭喜获得四等奖!");
+            prizeMsg.put("choice", 4);
+            prizeMsg.put("content", "恭喜获得四等奖! " + date);
         } else if (num > 310 && num <= 610) {
-            prizeMsg.put("msg","恭喜获得五等奖!");
-            prizeMsg.put("choice",5);
-            prizeMsg.put("content","恭喜获得五等奖! " + date);
+            prizeMsg.put("msg", "恭喜获得五等奖!");
+            prizeMsg.put("choice", 5);
+            prizeMsg.put("content", "恭喜获得五等奖! " + date);
         } else if (num > 610 && num <= 1110) {
-            prizeMsg.put("msg","恭喜获得六等奖!");
-            prizeMsg.put("choice",6);
-            prizeMsg.put("content","恭喜获得六等奖! " + date);
+            prizeMsg.put("msg", "恭喜获得六等奖!");
+            prizeMsg.put("choice", 6);
+            prizeMsg.put("content", "恭喜获得六等奖! " + date);
         } else {
-            prizeMsg.put("msg","未中奖!");
-            prizeMsg.put("choice",0);
-            prizeMsg.put("content","未中奖! " + date);
+            prizeMsg.put("msg", "未中奖!");
+            prizeMsg.put("choice", 0);
+            prizeMsg.put("content", "未中奖! " + date);
         }
         return prizeMsg;
     }
 
-    /*public static void main(String[] args) {
+    public static void main(String[] args) {
+        //数据库操作demo
+        //读取已启用的奖品模板以模板及总量
+        List<Prize> prizeList=null;//"select * from prize where enable=1";//获取list
+        Integer size=prizeList.size();//获取list总量
+        Integer remains=0;//剩余奖品总量
+        for (Prize prize:prizeList){
+            remains+=prize.getRemain();
+        }
+
+
+
+
         int num = (int) (Math.random() * 10000 + 1);//产生1~10001随机数 [1,10001)
         if (num == 1) {
             System.out.println(num+"恭喜获得一等奖!");
@@ -86,5 +99,5 @@ public class PrizeController {
         } else {
             System.out.println(num+"未中奖!");
         }
-    }*/
+    }
 }
--- src/main/java/com/myzonespringboot/dao/imp/BaseDaoImp.java
+++ src/main/java/com/myzonespringboot/dao/imp/BaseDaoImp.java
@@ -1,65 +1,347 @@
 package com.myzonespringboot.dao.imp;
 
 import com.myzonespringboot.dao.IBaseDao;
-import org.hibernate.SessionFactory;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.hibernate.*;
+import org.springframework.orm.hibernate4.HibernateCallback;
+import org.springframework.orm.hibernate4.support.HibernateDaoSupport;
 import org.springframework.stereotype.Repository;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.Resource;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by cgq on 2017/10/26.
  */
 @Repository("baseDaoImp")
-public class BaseDaoImp<T> implements IBaseDao<T> {
+public class BaseDaoImp extends HibernateDaoSupport implements IBaseDao  {
 
-    @Autowired
+    @Resource
     private SessionFactory sessionFactory;
 
-    private Class<T> clazz; //T的具体类
+    @PostConstruct
+    public void initSessionFactory() {
+        super.setSessionFactory(sessionFactory);
+    }
 
     /**
-     * 通过构造方法指定DAO的具体实现类
+     *
+     * 功能描述: 获取多个结果
+     *
+     * @param hql
+     * @return
      */
-    @SuppressWarnings("unchecked")
-    public BaseDaoImp() {
-        /*ParameterizedType type = (ParameterizedType) this.getClass().getGenericSuperclass();
-        clazz = (Class<T>) type.getActualTypeArguments()[0];*/
-        //System.out.println("DAO的真实实现类是："+this.clazz.getName());
+    @Override
+    public List getManyObjects(String hql) {
+        return getHibernateTemplate().find(hql);
+    }
+
+    /**
+     *
+     * 功能描述: 预编译,带参数,得到多条记录
+     *
+     * @param hql
+     * @param args
+     * @return
+     */
+    @Override
+    public List getManyObjects(String hql, Object[] args) {
+        return getHibernateTemplate().find(hql, args);
+    }
+
+    /**
+     *
+     * 功能描述: 预编译,带参数，得到一条记录
+     *
+     * @param hql
+     * @param args
+     * @return
+     */
+    @Override
+    public Object getOneObject(String hql, Object[] args) {
+        List list = getHibernateTemplate().find(hql, args);
+        if (list == null || list.size() == 0) {
+            return null;
+        } else {
+            return list.get(0);
+        }
+    }
+
+    /**
+     *
+     * 功能描述: 保存一条记录
+     *
+     * @param obj
+     * @return
+     */
+    @Override
+    public Object save(Object obj) {
+        return getHibernateTemplate().save(obj);
+    }
+
+    /**
+     *
+     * 功能描述: 保存多条记录
+     *
+     * @param objs
+     * @return
+     */
+    @Override
+    public List saveAll(List objs) {
+        if (objs == null || objs.size() == 0) {
+            return null;
+        }
+        List list = new ArrayList();
+        for (Object ob : objs) {
+            list.add(save(ob));
+        }
+        return list;
+    }
+
+    /**
+     *
+     * 功能描述: 更新一条实体
+     *
+     * @param obj
+     * @return
+     */
+    @Override
+    public void update(Object obj) {
+        getHibernateTemplate().update(obj);
+    }
+
+    /**
+     *
+     * 功能描述: 更新/保存一条实体
+     *
+     * @param obj
+     * @return
+     */
+    @Override
+    public void merge(Object obj) {
+        getHibernateTemplate().merge(obj);
+    }
+
+    /**
+     *
+     * 功能描述: 更新/保存一条实体
+     *
+     * @param obj
+     * @return
+     */
+    @Override
+    public void saveOrUpdate(Object obj) {
+        getHibernateTemplate().saveOrUpdate(obj);
+    }
+
+    /**
+     *
+     * 功能描述: 删除实体
+     *
+     * @param obj
+     */
+    @Override
+    public void delete(Object obj) {
+        getHibernateTemplate().delete(obj);
+    }
+
+    /**
+     *
+     * 功能描述:保存集合对象
+     *
+     * @param list
+     */
+    @Override
+    public void saveManyObjects(List list) {
+        if (list == null || list.size() == 0) {
+            return;
+        }
+        for (int i = 0; i < list.size(); i++) {
+            getHibernateTemplate().save(list.get(i));
+        }
+    }
+
+    /**
+     * 功能描述: 执行HQL
+     *
+     * @param hql
+     * @return
+     */
+    @Override
+    public int bulkUpdate(String hql) {
+        return getHibernateTemplate().bulkUpdate(hql);
+    }
+
+    /**
+     *
+     * 功能描述: 执行HQL,带参数
+     *
+     * @param hql
+     * @param params
+     * @return
+     */
+    @Override
+    public int bulkUpdate(String hql, final Object[] params) {
+        return getHibernateTemplate().bulkUpdate(hql, params);
+    }
+
+    /**
+     *
+     * 功能描述: 执行原生增删改SQL
+     *
+     * @param sql
+     */
+    @Override
+    public void executeNativeSqlUpdate(final String sql) {
+        this.getHibernateTemplate().execute(new HibernateCallback() {
+            public Object doInHibernate(Session session) {
+                session.createSQLQuery(sql).executeUpdate();
+                return null;
+            }
+        });
+    }
+
+    /**
+     *
+     * 功能描述: 执行原生增删改SQL 带参数
+     *
+     * @param sql
+     * @param params
+     */
+    @Override
+    public void executeNativeSqlUpdate(final String sql, final Object[] params) {
+        this.getHibernateTemplate().execute(new HibernateCallback() {
+            public Object doInHibernate(Session session) {
+                Query query = session.createSQLQuery(sql);
+                if (params != null && params.length > 0) {
+                    for (int i = 0; i < params.length; i++) {
+                        query.setParameter(i, params[i]);
+                    }
+                }
+                query.executeUpdate();
+                return null;
+            }
+        });
+    }
+
+    /**
+     *
+     * 功能描述: 执行原生查询SQL,带参数
+     *
+     * @param sql
+     * @return
+     */
+    @Override
+    public List executeNativeSqlQuery(final String sql, final Object[] params) {
+        List list = (List) this.getHibernateTemplate().execute(new HibernateCallback() {
+            public Object doInHibernate(Session session) {
+                Query query = session.createSQLQuery(sql);
+                if (params != null && params.length > 0) {
+                    for (int i = 0; i < params.length; i++) {
+                        query.setParameter(i, params[i]);
+                    }
+                }
+                return query.list();
+            }
+        });
+        return list;
     }
 
     @Override
-    public void save(T entity) {
-
-    }
-
-    @Override
-    public void update(T entity) {
-
-    }
-
-    @Override
-    public void partUpdate(Long id, String[] names, Object[] values) {
-
-    }
-
-    @Override
-    public void delete(Long id) {
-
-    }
-
-    @Override
-    public T findById(Long id) {
+    public List findByPage(String sql, int firstRow, int maxRow) {
         return null;
     }
 
-    @Override
-    public List<T> findByHQL(String hql, Object... params) {
-        return null;
+    /**
+     *
+     * 功能描述: 分页查询
+     *
+     * @param sql
+     * @param firstRow
+     * @param maxRow
+     * @return
+     */
+    /*public List findByPage(final String sql, final int firstRow, final int maxRow) {
+        return getHibernateTemplate().executeFind(new HibernateCallback() {
+
+            public Object doInHibernate(Session session) {
+                Query q = session.createQuery(sql);
+                q.setFirstResult(firstRow);
+                q.setMaxResults(maxRow);
+                return q.list();
+            }
+
+        });
+    }*/
+
+    /**
+     *
+     * 功能描述: 获取总页数
+     *
+     * @param sql
+     * @return
+     */
+    public int findTotal(final String sql) {
+        Long total = (Long) getHibernateTemplate().execute(new HibernateCallback() {
+            public Object doInHibernate(Session session) {
+                String countHQL = "select count(1) " + sql;
+                Query q = session.createQuery(countHQL);
+                Long total = (Long) q.uniqueResult();
+                return total;
+            }
+
+        });
+        if (total != null) {
+            return total.intValue();
+        } else {
+            return 0;
+        }
+    }
+
+    /**
+     * 根据指定的原生SQL和参数 查询 返回对应的java实体
+     * @param sql 原生SQL查询语句
+     * @param params SQL参数数组
+     * @param clazz 实体类
+     * @return List
+     */
+    public List executeNativeSqlQueryForClass(final String sql, final Object[] params, final Class clazz) {
+        List list = (List) this.getHibernateTemplate().execute(new HibernateCallback() {
+            public Object doInHibernate(Session session) throws HibernateException {
+                Query query = session.createSQLQuery(sql).addEntity(clazz);
+                if (params != null && params.length > 0) {
+                    for (int i = 0; i < params.length; i++) {
+                        query.setParameter(i, params[i]);
+                    }
+                }
+                return query.list();
+            }
+        });
+        return list;
     }
 
     @Override
-    public List<T> queryPage(String hql, int pageNo, int pageSize) {
+    public List findMapBySql(String sql) {
         return null;
     }
+
+    /**
+     *
+     * 功能描述:执行原生sql 查询 返回List<map>的 结构
+     *
+     * @param sql
+     * @return
+     */
+    /*public List findMapBySql(final String sql) {
+        return getHibernateTemplate().executeFind(new HibernateCallback() {
+
+            public Object doInHibernate(Session session) {
+                SQLQuery query = session.createSQLQuery(sql);
+                query.setResultTransformer(CriteriaSpecification.ALIAS_TO_ENTITY_MAP);
+                return query.list();
+            }
+
+        });
+    }*/
 }
